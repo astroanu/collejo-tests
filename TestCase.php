47bd81d5b85af40ba0f8e40339cb75e785ab530f
Anuradha <astroanu2004@gmail.com>
2016-08-23 23:20:38 +0530

    added tests

---
+++ TestCase.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Tests;
+
+use Illuminate\Foundation\Testing\TestCase as IlluminateTestCase;
+use Collejo\Core\Contracts\Console\Kernel;
+use Collejo\Core\Contracts\Http\Kernel as Http;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Http\Request;
+
+abstract class TestCase extends IlluminateTestCase
+{
+    use DatabaseMigrations;
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
+    public function call($method, $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null)
+    {
+        $kernel = $this->app->make(Http::class);
+
+        $this->currentUri = $this->prepareUrlForRequest($uri);
+
+        $this->resetPageContext();
+
+        $request = Request::create(
+            $this->currentUri, $method, $parameters,
+            $cookies, $files, array_replace($this->serverVariables, $server), $content
+        );
+
+        $response = $kernel->handle($request);
+
+        $kernel->terminate($request, $response);
+
+        return $this->response = $response;
+    }
+
+    public function artisan($command, $parameters = [])
+    {
+        return $this->code = $this->app[Kernel::class]->call($command, $parameters);
+    } 
+
+    public function assertArrayValuesEquals($a, $b)
+    {
+        $a = $a->toArray();
+        $b = $b->toArray();
+
+        if (isset($a['id'])) { unset($a['id']); }        
+
+        if (isset($b['id'])) { unset($b['id']); }
+
+        foreach($a as $k => $v) {
+            if (isset($b[$k])) {
+                if ($v !== $b[$k]) {
+                    return $this->assertTrue(false);
+                }
+            }
+        } 
+        
+        return $this->assertTrue(true);       
+    }
+
+    public function assertArrayEquals($a, $b) {
+        $a = $a->toArray();
+        $b = $b->toArray();
+
+        if (isset($a['id'])) { unset($a['id']); }        
+
+        if (isset($b['id'])) { unset($b['id']); }
+
+        if (count(array_diff_assoc($a, $b))) {
+            return $this->assertTrue(false);
+        }
+
+        foreach($a as $k => $v) {
+            if ($v !== $b[$k]) {
+                return $this->assertTrue(false);
+            }
+        }
+
+        return $this->assertTrue(true);
+    }          
+}
---
+++ classes/BatchTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Tests;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Collejo\Core\Contracts\Repository\ClassRepository;
+use Collejo\App\Models\Batch;
+
+class BatchTest extends TestCase
+{
+    use DatabaseMigrations, DatabaseTransactions;
+
+    private $classRepository;
+
+    public function testBatchCreate()
+    {
+        $batch = factory(Batch::class)->create();
+
+        $model = $this->classRepository->createBatch($batch->toArray());
+
+        $this->assertArrayValuesEquals($model, $batch);
+    }
+
+    public function testBatchUpdate()
+    {
+        $batch = factory(Batch::class)->create();
+        $batchNew = factory(Batch::class)->make();
+
+        $model = $this->classRepository->updateBatch($batchNew->toArray(), $batch->id);
+
+        $this->assertArrayValuesEquals($model, $batchNew);
+    }    
+
+    public function setup()
+    {
+        parent::setup();
+        
+        $this->classRepository = $this->app->make(ClassRepository::class);
+    }
+}
